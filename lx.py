ad114a4c3a965fe56ddcbca66f2fb1f3ac1e1bd6
HOU <18322350009@163.com>
2022-06-17 18:58:14 +0800

    测试hello

--- lx.py
+++ lx.py
@@ -1,5 +1,10 @@
 print("我的分支")
 a = 2 < 4 and 2 == 4
 a=3<4 and 3 == 4
-5 = 7 < 8 and 5 == 9
+
 print(a)
+grood_list = ["Andy","Tom"]
+for i in grood_list:
+    print(i+",Welcome to School!")
+print("123测试")
+print("text1")
